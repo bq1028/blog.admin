c739eb9b8605099315002823be3714d5ac0738ef
Steudnera C. Koch <wowcxy2008@126.com>
2018-07-03 16:06:23 +0800

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,69 @@
+/**
+ * 用户模型
+ * @author Philip
+ */
 
+"use strict";
+
+let Sequelize = require('sequelize');
+let sequelize = require('./../sequelize');
+
+var role = require('./role');
+
+var user = sequelize.define('user', {
+    username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: false
+    },
+    nick: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+    },
+    mobile: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+    },
+    avatar: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        unique: false
+    },
+    birth: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        unique: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        primaryKey: false
+    },
+    roleId: {
+        type: Sequelize.INTEGER,
+
+        references: {
+            model: role,
+            key: 'id'
+        }    
+    }    
+}, {
+    paranoid: false,
+    timestamps: true,
+    underscored: true,
+    freezeTableName: true
+});
+
+module.exports = user;
