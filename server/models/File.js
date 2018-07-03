248920c35330d60f7991b81b1c907380ac004ae8
Steudnera C. Koch <wowcxy2008@126.com>
2018-07-03 16:03:23 +0800

    Create File.js

---
+++ server/models/File.js
@@ -0,0 +1,49 @@
+/**
+ * 文件模型
+ * @author Philip
+ */
+
+"use strict";
+
+let Sequelize = require('sequelize');
+let sequelize = require('./../sequelize');
+
+var user = require('./user');
+
+var file = sequelize.define('file', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: false
+    },
+    type: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: false
+    },
+    ext: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: false
+    },
+    url: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: false
+    },
+    userId: {
+        type: Sequelize.INTEGER,
+
+        references: {
+            model: user,
+            key: 'id'
+        }   
+    }    
+}, {
+    paranoid: false,
+    timestamps: true,
+    underscored: true,
+    freezeTableName: true
+});
+
+module.exports = file;
