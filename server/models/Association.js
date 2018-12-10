35b1843eb1ed838237b1f7dbf170b7e443d8dee9
Philip <wowcxy2008@126.com>
2018-12-10 11:49:22 +0800

    Update Association.js

--- server/models/Association.js
+++ server/models/Association.js
@@ -1,23 +1,28 @@
-var sequelize = require('./sequelize');
+/**
+ * 模型关联
+ * @author Philip
+ */
+"use strict"
 
-var authority = require('./schemas/authority');
-var role = require('./schemas/role');
-var user = require('./schemas/user');
-var file = require('./schemas/file');
-var tag = require('./schemas/tag');
-var content = require('./schemas/content');
-var message = require('./schemas/message');
-var journal = require('./schemas/journal');
-var attachment = require('./schemas/attachment');
+const sequelize = require('./sequelize')
 
-var roleAuth = require('./schemas/role-authority');
+const authority = require('./schemas/authority')
+const role = require('./schemas/role')
+const user = require('./schemas/user')
+const file = require('./schemas/file')
+const tag = require('./schemas/tag')
+const content = require('./schemas/content')
+const message = require('./schemas/message')
+const journal = require('./schemas/journal')
+const attachment = require('./schemas/attachment')
+const roleAuth = require('./schemas/role-authority')
 
-module.exports.init = function() {
+module.exports.init = function () {
   authority.belongsToMany(role, { 
     through: roleAuth, 
     foreignKey: 'roleId', 
     as: 'roles'
-  });
+  })
 
   role.belongsToMany(authority, { 
     through: roleAuth, 
@@ -29,7 +34,7 @@ module.exports.init = function() {
     foreignKey: 'roleId',
     constraints: false,
     as: 'role'
-  });
+  })
 
   journal.belongsTo(user, {
     foreignKey: 'roleId',
@@ -41,23 +46,23 @@ module.exports.init = function() {
     foreignKey: 'contentId',
     constraints: false,
     as: 'attachments'
-  });
+  })
 
   content.hasMany(message, {
     foreignKey: 'contentId',
     constraints: false,
     as: 'messages'
-  });
+  })
 
   attachment.hasOne(file, {
     foreignKey: 'fileId',
     constraints: false,
     as: 'file'
-  });
+  })
 
   attachment.belongsTo(user, {
     foreignKey: 'userId',
     constraints: false,
     as: 'owner'
-  });
+  })
 }
