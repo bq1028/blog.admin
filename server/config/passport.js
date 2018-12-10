b8f51b8ef323c75ce2a24270d96a9fbc7a1a7c8e
Philip <wowcxy2008@126.com>
2018-12-10 11:40:50 +0800

    Update passport.js

--- server/config/passport.js
+++ server/config/passport.js
@@ -8,18 +8,18 @@ const LocalStrategy = require("passport-local").Strategy
 const authenticator = require("../libs/authenticator")
 const user = require('../models/schemas/user')
 
-const serialize = function(user, done) {
+const serialize = function (user, done) {
     done(null, user.id)
 }
 
-const deserialize = function(id, done) {
-    user.findById(id).then(function(data) {
+const deserialize = function (id, done) {
+    user.findById(id).then(function (data) {
         done(null, data)
     })
 }
 
 module.exports = function (passport, config) {
-    passport.serializeUser(serialize);
-    passport.deserializeUser(deserialize);
-    passport.use(new LocalStrategy(authenticator.localUser));
-};
+    passport.serializeUser(serialize)
+    passport.deserializeUser(deserialize)
+    passport.use(new LocalStrategy(authenticator.localUser))
+}
