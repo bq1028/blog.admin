1ec27d3e1bb86a0e3ccab8f8f38dfc5c07ec3f03
Philip <wowcxy2008@126.com>
2018-12-10 11:30:28 +0800

    Update action.js

--- server/config/action.js
+++ server/config/action.js
@@ -3,7 +3,7 @@
  * @author Philip
  */
 
-var actions = {
+module.exports.actions = {
     "VISIT": "访问",
     "ADD": "增加",
     "MODIFIED": "修改",
@@ -11,32 +11,32 @@ var actions = {
     "SEARCH": "查询",
     "LOGIN": "登录",
     "LOGOUT": "登出"
-};
+}
 
-var pages = {
-    ARTICLES: '文章',
-    HOME: '首页',
-    LOGIN: '登录',
-    LOGOUT: '登出',
-    PROJECTS: '计划',
-    REGISTER: '注册',
-    SERVICES: '服务',
-    USER: '用户'
-};
+module.exports.pages = {
+    ARTICLES: "文章",
+    HOME: "首页",
+    LOGIN: "登录",
+    LOGOUT: "登出",
+    PROJECTS: "计划",
+    REGISTER: "注册",
+    SERVICES: "服务",
+    USER: "用户"
+}
 
-var models = {
-    AUTH: '权限',
-    CONTENT: '内容',
-    FILE: '文件',
-    JOURNAL: '日志',
-    MESSAGE: '留言',
-    ROLE: '角色',
-    TAG: '标签',
-    USER: '用户'
-};
+module.exports.models = {
+    AUTH: "权限",
+    CONTENT: "内容",
+    FILE: "文件",
+    JOURNAL: "日志",
+    MESSAGE: "留言",
+    ROLE: "角色",
+    TAG: "标签",
+    USER: "用户"
+}
 
-var resultType = {
-    NOAUTH: '没有权限',
-    SQLFIALED: '数据库错误',
-    SUCCESS: '成功'
-};
+module.exports.resultType = {
+    NOAUTH: "没有权限",
+    SQLFIALED: "数据库错误",
+    SUCCESS: "成功"
+}
