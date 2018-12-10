0a47c01edf69ab98c806c323d67a447d40afc25d
Philip <wowcxy2008@126.com>
2018-12-10 11:39:45 +0800

    Update passport.js

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,19 +1,24 @@
-"use strict";
-var LocalStrategy = require("passport-local").Strategy;
-var authenticator = require("../libs/authenticator");
-var user = require('../models/schemas/user')
+/**
+ * 登录状态校验
+ * @author Philip 
+ */
+"use strict"
 
-var serialize = function(user, done) {
-    done(null, user.id);
-};
+const LocalStrategy = require("passport-local").Strategy
+const authenticator = require("../libs/authenticator")
+const user = require('../models/schemas/user')
 
-var deserialize = function(id, done) {
+const serialize = function(user, done) {
+    done(null, user.id)
+}
+
+const deserialize = function(id, done) {
     user.findById(id).then(function(data) {
-        done(null, data);
-    });
-};
+        done(null, data)
+    })
+}
 
-module.exports = function(passport, config) {
+module.exports = function (passport, config) {
     passport.serializeUser(serialize);
     passport.deserializeUser(deserialize);
     passport.use(new LocalStrategy(authenticator.localUser));
