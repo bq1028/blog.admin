7c7d994e0acc1ea4a9235f798e1d16e3f554b233
Philip <wowcxy2008@126.com>
2018-12-10 11:37:52 +0800

    Update routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,27 +1,32 @@
+/**
+ * 路由设置
+ * @author Philip
+ */
 "use strict";
-var Router = require("koa-router");
 
-var index = require("../controllers/routes/index");
-var login = require("../controllers/routes/login");
+const Router = require("koa-router")
 
-var secured = async function (ctx, next) {
+const index = require("../controllers/routes/index")
+const login = require("../controllers/routes/login")
+
+const secured = async function (ctx, next) {
     if (ctx.isAuthenticated()) {
-        await next ();
+        await next ()
     } else {
-        ctx.redirect('/login');
+        ctx.redirect('/login')
     }
-};
+}
 
-var unsecured = async function (ctx, next) {
+const unsecured = async function (ctx, next) {
     if (!ctx.isAuthenticated()) {
-        await next ();
+        await next ()
     } else {
-        ctx.redirect('/');
+        ctx.redirect('/')
     }
-};
+}
 
 module.exports = function (app, passport) {
-    var router = new Router();
+    let router = new Router();
 
     router.get("/", secured, async function (ctx, next) {
         ctx.type = "html";
@@ -29,19 +34,19 @@ module.exports = function (app, passport) {
     });
     
     router.get(["/journal", "/user", "/content", "/authority"], secured, async function (ctx, next) {
-        ctx.type = "html";
-        await index.apply(ctx);
-    });
+        ctx.type = "html"
+        await index.apply(ctx)
+    })
 
     router.get("/login", unsecured, async function (ctx, next) {
-        ctx.type = "html";
-        await login.apply(ctx);
-    });
+        ctx.type = "html"
+        await login.apply(ctx)
+    })
 
     router.get("/logout", secured, async function (ctx, next) {
-        ctx.session = null;
-        ctx.redirect('/login');
-    });    
+        ctx.session = null
+        ctx.redirect('/login')
+    })
 
-    app.use(router.routes());
-};
+    app.use(router.routes())
+}
