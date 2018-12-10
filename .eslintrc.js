c2e4cf8eadf43e9f501d98deedc2dbb9676a852e
Philip <wowcxy2008@126.com>
2018-12-10 11:13:16 +0800

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+/**
+ * Eslint 配置
+ * @author Philip
+ */
+
+module.exports = {
+  root: true,
+  
+  parserOptions: {
+    sourceType: 'module'
+  },
+  
+  env: {
+    browser: true,
+  },
+  
+  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+  extends: 'standard',
+  
+  // 全局变量
+  globals: {
+    $: true,
+    window: true,
+  },
+  
+  // add your custom rules here
+  rules: {
+    "indent": ["error", 4],
+    "generator-star-spacing": "off",
+    "no-debugger": process.env.NODE_ENV === ' production' ? 'error' : 'off'
+  }
+}
