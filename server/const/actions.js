425d330a404f8110ba89355179d81c5636d3962f
Philip <wowcxy2008@126.com>
2018-12-11 14:48:06 +0800

    Create actions.js

---
+++ server/const/actions.js
@@ -0,0 +1,14 @@
+/**
+ * 行为
+ * @author Philip
+ */
+
+module.exports.actions = {
+    "VISIT": "访问",
+    "ADD": "增加",
+    "MODIFIED": "修改",
+    "DELETE": "删除",
+    "SEARCH": "查询",
+    "LOGIN": "登录",
+    "LOGOUT": "登出"
+}
