18d9150db4a330cf860195a71dea88dbf14bcca9
Philip <wowcxy2008@126.com>
2018-12-11 14:48:24 +0800

    Create pages.js

---
+++ server/const/pages.js
@@ -0,0 +1,10 @@
+module.exports.pages = {
+    ARTICLES: "文章",
+    HOME: "首页",
+    LOGIN: "登录",
+    LOGOUT: "登出",
+    PROJECTS: "计划",
+    REGISTER: "注册",
+    SERVICES: "服务",
+    USER: "用户"
+}
