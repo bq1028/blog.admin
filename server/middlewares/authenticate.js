f5390d5235e93c3284b90367c7e09485ac83a9a7
Philip <wowcxy2008@126.com>
2018-12-11 15:12:55 +0800

    Update and rename Authenticate.js to authenticate.js

--- server/middlewares/authenticate.js
+++ server/middlewares/authenticate.js
@@ -3,13 +3,10 @@ import jwt from 'jsonwebtoken'
 export default function (ctx) {
     ctx.status = 200
     const { state } = ctx
+    
     return ctx.body = {
         uid: state,
-        token: jwt.sign(
-            { user: state },
-            'Mason',
-            { expiresIn: '2 days' }
-        ),
+        token: jwt.sign({ user: state }, 'Mason', { expiresIn: '2 days' }),
         message: '登陆成功'
     }
 }
