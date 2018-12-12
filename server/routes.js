e5632374c16d7ef5c15ef03f29db7fb089c0d1d2
Philip <wowcxy2008@126.com>
2018-12-12 14:32:35 +0800

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -5,16 +5,13 @@
 "use strict"
 
 const Router = require("koa-router")
-
-// 控制器
-const index = require("../controllers/routes/index")
-const login = require("../controllers/routes/login")
+const render = require('koa-ejs')
 
 const secured = async function (ctx, next) {
     if (ctx.isAuthenticated()) {
         await next ()
     } else {
-        ctx.redirect('/login')
+        ctx.redirect("/login")
     }
 }
 
@@ -22,32 +19,37 @@ const unsecured = async function (ctx, next) {
     if (!ctx.isAuthenticated()) {
         await next ()
     } else {
-        ctx.redirect('/')
+        ctx.redirect("/")
     }
 }
 
 module.exports = function (app, passport) {
-    let router = new Router();
+    let router = new Router()
 
     router.get("/", secured, async function (ctx, next) {
-        ctx.type = "html"
         await index.apply(ctx)
     })
     
     router.get(["/journal", "/user", "/content", "/authority"], secured, async function (ctx, next) {
-        ctx.type = "html"
         await index.apply(ctx)
     })
 
     router.get("/login", unsecured, async function (ctx, next) {
-        ctx.type = "html"
         await login.apply(ctx)
     })
 
     router.get("/logout", secured, async function (ctx, next) {
-        ctx.session = null
-        ctx.redirect('/login')
+        ctx.redirect("/login")
     })
 
+    render(app, {
+        root: path.join(__dirname, '../web/dist'),
+        layout: 'template',
+        viewExt: 'html',
+        cache: false,
+        debug: true
+    })
+
+    app.use(views(__dirname, { map: {html: 'nunjucks' }}))
     app.use(router.routes())
 }
