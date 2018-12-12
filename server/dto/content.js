3e080c24d7af27f05d49e5af43d09de3c9d9ebda
Philip <wowcxy2008@126.com>
2018-12-12 15:13:29 +0800

    Update content.js

--- server/dto/content.js
+++ server/dto/content.js
@@ -1,1 +1,63 @@
+/**
+ * 内容模型
+ * @author Philip
+ */
+
+const Sequelize = require('sequelize')
+const sequelize = require('../sequelize')
+
+// models
+const user = require('./user')
+const authority = require('./authority')
+
+const content = sequelize.define('content', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: false
+    },
+    keywords: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        unique: false
+    },       
+    type: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: false
+    },
+    content: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        unique: false
+    },
+    tags: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: false        
+    },
+    authorityId: {
+        type: Sequelize.INTEGER,
+
+        references: {
+            model: authority,
+            key: 'id'
+        }
+    },
+    userId: {
+        type: Sequelize.INTEGER,
+
+        references: {
+            model: user,
+            key: 'id'
+        }
+    }      
+}, {
+    paranoid: false,
+    timestamps: true,
+    underscored: true,
+    freezeTableName: true
+})
+
+module.exports = content
 
