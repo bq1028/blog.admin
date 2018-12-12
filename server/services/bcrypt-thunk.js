9df82614853a541b3a1e8615586e2e35b96771d0
Philip <wowcxy2008@126.com>
2018-12-12 14:39:36 +0800

    Update bcrypt-thunk.js

--- server/services/bcrypt-thunk.js
+++ server/services/bcrypt-thunk.js
@@ -3,55 +3,59 @@
  * @author Philip
  */
 
-"use strict";
-var bcrypt = require("bcrypt");
+"use strict"
 
-module.exports = bcrypt.genSaltSync;
-module.exports = bcrypt.hashSync;
-module.exports = bcrypt.compareSync;
-module.exports.getRounds = bcrypt.getRounds;
+var bcrypt = require("bcrypt")
+
+module.exports = bcrypt.hashSync
+module.exports = bcrypt.compareSync
+module.exports = bcrypt.genSaltSync
+module.exports.getRounds = bcrypt.getRounds
 
 /**
  * hash化
  * @returns { Promise } hash化promise对象
  */
-module.exports.genSalt = function(rounds, ignore) {
-    return new Promise(function(resolve, reject) {
-        bcrypt.genSalt(rounds, ignore, function(err, salt) {
+module.exports.genSalt = function (rounds, ignore) {
+    return new Promise(function (resolve, reject) {
+        bcrypt.genSalt(rounds, ignore, function (err, salt) {
             if (err) {
-                return reject(err);
+                return reject(err)
             }
-            return resolve(salt);
-        });
-    }); 
-};
+            
+            return resolve(salt)
+        })
+    })
+}
 
 /**
  * hash化
  * @returns { Promise } hash化promise对象
  */
-module.exports.hash = function(data, salt) {
-    return new Promise(function(resolve, reject) {
-        bcrypt.hash(data, salt, function(err, hash) {
+module.exports.hash = function (data, salt) {
+    return new Promise(function (resolve, reject) {
+        bcrypt.hash(data, salt, function (err, hash) {
             if (err) {
-                return reject(err); 
+                return reject(err)
             } 
-            return resolve(hash);
-        });
-    });
-};
+            
+            return resolve(hash)
+        })
+    })
+}
 
 /**
  * 校验hash化的值
  * @returns { Promise } 校验的promise对象
  */
-module.exports.compare = function(data, hash) {
-    return new Promise(function(resolve, reject) {
+module.exports.compare = function (data, hash) {
+    return new Promise(function (resolve, reject) {
         bcrypt.compare(data, hash, function(err, matched) {
             if (err) {
-                return reject(err);
+                return reject(err)
             }
-            return resolve(matched);
-        });
-    });
-};
+            
+            return resolve(matched)
+        })
+    })
+}
