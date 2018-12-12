b6de1a43422937eeb659193ed5c50cf8025918ce
Philip <wowcxy2008@126.com>
2018-12-12 14:44:37 +0800

    Update attachment.js

--- server/controllers/attachment.js
+++ server/controllers/attachment.js
@@ -3,144 +3,107 @@
  * @author Philip
  */
 
-import 'babel-polyfill'
-import User from '../dao/users'
+import "babel-polyfill"
+import attachmentDao from "../dao/attachment"
 
 class Attachment {
     /**
-     * Get all users
-     * @param {ctx} Koa Context
+     * 获取全部附件
+     * @param {Context} context
      */
-    async find(ctx) {
+    async find (ctx) {
         ctx.body = await User.find()
     }
 
     /**
-     * Find a user
-     * @param {ctx} Koa Context
+     * 根据id查询
+     * @param {Context} Koa Context
      */
-    async findById(ctx) {
+    async findById (ctx) {
         try {
-          const user = await User.findById(ctx.params.id)
+            const user = await User.findById(ctx.params.id)
 
-          if (!user) {
-            ctx.throw(404)
-          }
-
-          ctx.body = user
-        } catch (err) {
-          if (err.name === 'CastError' || err.name === 'NotFoundError') {
-            ctx.throw(404)
-          }
-
-          ctx.throw(500)
-        }
-    }
-
-    /**
-     * Add a user
-     * @param {ctx} Koa Context
-     */
-    async add(ctx) {
-        try {
-          const user = await new User(ctx.request.body).save()
-
-          ctx.body = {
-            retData: user,
-            message: '注册成功',
-          }
-        } catch (err) {
-          ctx.throw(err)
-        }
-    }
-
-    /**
-     * Update a user
-     * @param {ctx} Koa Context
-     */
-    async update(ctx) {
-        try {
-          const user = await User.findByIdAndUpdate(ctx.params.id,
-            { ...ctx.request.body, updated_at: Date.now() })
-          if (!user) {
-            ctx.throw(404)
-          }
-
-          const updated = await User.findById(ctx.params.id)
-          ctx.body = updated
-        } catch (err) {
-          if (err.name === 'CastError' || err.name === 'NotFoundError') {
-            return ctx.throw(err, 404)
-          }
-
-          ctx.throw(err.message, 500)
-        }
-    }
-
-    /**
-     * Delete a user
-     * @param {ctx} Koa Context
-     */
-    async delete(ctx) {
-        try {
-          const user = await User.findByIdAndRemove(ctx.params.id)
-
-          if (!user) {
-            ctx.status = 405
-
-            return ctx.body = {
-              message: '查无此账号'
+            if (!user) {
+                ctx.throw(404)
             }
-          }
-          ctx.body = {
-            message: '删除用户成功'
-          }
-        } catch (err) {
-          if (err.name === 'CastError' || err.name === 'NotFoundError') {
-            ctx.throw(404)
-          }
 
-          ctx.throw(500)
+            ctx.body = user
+        } catch (err) {
+            if (err.name === "CastError" || err.name === "NotFoundError") {
+                ctx.throw(404)
+            }
+
+            ctx.throw(500)
         }
     }
 
     /**
-     * user login, get token
-     * @param {ctx} Koa Context
+     * 添加
+     * @param {Context} Koa Context
      */
-    async login(ctx, next) {
+    async add (ctx) {
         try {
-            const { username, password } = ctx.request.body
+            const user = await new User(ctx.request.body).save()
 
-            await User.findOne({ username }, (err, user) => {
-              if (!user) {
-                ctx.status = 400
+            ctx.body = {
+                retData: user,
+                message: "注册成功"
+            }
+        } catch (err) {
+            ctx.throw(err)
+        }
+    }
+
+    /**
+     * 更新
+     * @param {Context} Koa Context
+     */
+    async update (ctx) {
+        try {
+            const user = await User.findByIdAndUpdate(ctx.params.id, { ...ctx.request.body, updated_at: Date.now() })
+
+            if (!user) {
+                ctx.throw(404)
+            }
+
+            const updated = await User.findById(ctx.params.id)
+            ctx.body = updated
+        } catch (err) {
+            if (err.name === "CastError" || err.name === "NotFoundError") {
+                return ctx.throw(err, 404)
+            }
+
+            ctx.throw(err.message, 500)
+        }
+    }
+
+    /**
+     * 删除
+     * @param {Context} Context
+     */
+    async delete (ctx) {
+        try {
+            const user = await User.findByIdAndRemove(ctx.params.id)
+
+            if (!user) {
+                ctx.status = 405
 
                 return ctx.body = {
-                  message: '账号不存在'
+                    message: "查无此账号"
                 }
-              }
-              
-              user.comparePassword(password, (err, isMatch) => {
-                if (isMatch) {
-                  ctx.state = user._id
-                  return next()
-                }
+            }
 
-                ctx.status = 401
-                ctx.body = {
-                  message: '账户名和密码不匹配'
-                }
-              })
-            })
+            ctx.body = {
+                message: "删除用户成功"
+            }
         } catch (err) {
-          ctx.status = 401
-          ctx.body = {
-            message: '登陆失败'
-          }
+            if (err.name === "CastError" || err.name === "NotFoundError") {
+                ctx.throw(404)
+            }
+
+            ctx.throw(500)
         }
     }
-
 }
 
 module.exports = new Attachment()
