36ad2efb98f1b94c28f59c664926b3f8fe82e876
Philip <wowcxy2008@126.com>
2019-06-17 14:59:52 +0800

    Create file.js

---
+++ server/controller/file.js
@@ -0,0 +1,14 @@
+/**
+ * 文件
+ * @author Philip
+ */
+const Controller = require("./Controller")
+const fileDto = require('../dtos/file')
+
+class FileController extends Controller {
+    constructor (fileDto) {
+        super(fileDto)
+    }
+}
+
+module.exports = new FileController(fileDto)
