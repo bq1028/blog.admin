19b57e4f4e333c9291fae12b6723816e3bda18fb
Philip <wowcxy2008@126.com>
2019-06-17 15:00:14 +0800

    Create fileType.js

---
+++ server/controller/fileType.js
@@ -0,0 +1,14 @@
+/**
+ * 文件类型
+ * @author Philip
+ */
+const Controller = require("./Controller")
+const fileTypeDto = require('../dtos/fileType')
+
+class FileTypeController extends Controller {
+    constructor (fileTypeDto) {
+        super(fileTypeDto)
+    }
+}
+
+module.exports = new FileTypeController(fileTypeDto)
