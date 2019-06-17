16d6a903567b5f12b08ab5957b83262a8d27ba79
Philip <wowcxy2008@126.com>
2019-06-17 15:01:42 +0800

    Create tag.js

---
+++ server/controller/tag.js
@@ -0,0 +1,15 @@
+
+/**
+ * 标签
+ * @author Philip
+ */
+const Controller = require("./Controller")
+const tagDto = require('../dtos/tag')
+
+class TagController extends Controller {
+    constructor (tagDto) {
+        super(tagDto)
+    }
+}
+
+module.exports = new TagController(tagDto)
